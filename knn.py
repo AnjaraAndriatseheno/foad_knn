1dec54a691e21debac0e8f9c50c2420711d48db5
AnjaraAndriatseheno <nanjaraandria@gmail.com>
2026-05-20 15:00:51 +0200

    feature : knn class creation

---
+++ knn.py
@@ -0,0 +1,7 @@
+class Knn : 
+    def__init__(self, neighbors_number=3):
+        self.neighbors_number = neighbors_number
+        self.donnees_entrainement = None           
+        self.etiquettes = None
+
+
