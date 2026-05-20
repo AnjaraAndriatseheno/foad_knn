d63543e1f33a8196993203f2c6b025bcdb2ed9a9
AnjaraAndriatseheno <nanjaraandria@gmail.com>
2026-05-20 15:18:12 +0200

    feature : now we can calculate de distance between 2 points by euclidienne division

--- knn.py
+++ knn.py
@@ -7,5 +7,12 @@ class Knn :
     def read(self, training_data, training_labels):
         self.training_data   = training_data
         self.training_labels = training_labels
+    
+    def _compute_distance(self, point_a, point_b):
+        sum_of_squares = 0
+        for index in range(len(point_a)):
+            difference      = point_a[index] - point_b[index]
+            sum_of_squares += difference ** 2
+        return sum_of_squares ** 0.5
 
 
