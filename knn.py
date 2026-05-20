19675e87a34ab6be2571f66d7783afcc21b68bf3
AnjaraAndriatseheno <nanjaraandria@gmail.com>
2026-05-20 15:08:37 +0200

    now we have the fit method for knn

--- knn.py
+++ knn.py
@@ -1,7 +1,11 @@
 class Knn : 
-    def__init__(self, neighbors_number=3):
+    def __init__(self, neighbors_number=3):
         self.neighbors_number = neighbors_number
         self.donnees_entrainement = None           
         self.etiquettes = None
+    
+    def read(self, training_data, training_labels):
+        self.training_data   = training_data
+        self.training_labels = training_labels
 
 
