f390efdbab118077aa82fcb2f6521c60f1b36b0a
AnjaraAndriatseheno <nanjaraandria@gmail.com>
2026-05-20 15:28:37 +0200

    feature : now knn can calculate the distance with all point, take the 3 most close, and make a vote for the prediction of the point type by majority

--- knn.py
+++ knn.py
@@ -18,4 +18,18 @@ class Knn :
         return sum_of_squares ** 0.5
 
 
-    
+    def _predict_one_point(self, unknown_point): 
+        distances_list = []
+        for index, known_point in enumerate(self.training_data):
+            distance = self._compute_distance(unknown_point, known_point)
+            distances_list.append((distance, self.training_labels[index]))
+            
+        distances_list.sort(key=lambda pair: pair[0])
+        nearest_neighbors = distances_list[:self.neighbors_number]
+
+        votes_counter = {}
+        for distance, label in nearest_neighbors:
+            votes_counter[label] = votes_counter.get(label, 0) + 1
+            
+        winning_class = max(votes_counter, key=votes_counter.get)
+        return winning_class
